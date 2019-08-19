e08b73a2a2e8f0ffcea356a1c0c1719c0acaf3ba
dantalianSVP <spenceronepro88@gmail.com>
2019-08-19 18:10:27 +0300

    Выполнена декомпозиция
    (упало в ошибку)

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,22 +1,22 @@
 
 from model.group import Group
-from fixture.application import  Application
+from fixture.application import Application
 import pytest
 
-@pytest.fixture()
+@pytest.fixture
 def app(request):
     fixture = Application
     request.addfinalizer(fixture.destroy)
     return fixture
 
 
-def test_add_group(self):
-    self.app.login(username="admin", password="secret")
-    self.app.create_group( Group(name="One Group", header="MARK", footer="Farenheit"))
-    self.app.logout()
+def test_add_group(app):
+    app.session.login(username="admin", password="secret")
+    app.create_group( Group(name="One Group", header="MARK", footer="Farenheit"))
+    app.session.logout()
 
-def test_empty_group(self):
-    self.app.login(username="admin", password="secret")
-    self.app.create_group( Group(name=" ", header=" ", footer=" "))
-    self.app.logout()
+#def test_empty_group(app):
+#    app.session.login(username="admin", password="secret")
+#    app.create_group( Group(name=" ", header=" ", footer=" "))
+#    app.session.logout()
 
