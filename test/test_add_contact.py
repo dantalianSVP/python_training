1e83d47fd2f4a01ce756b7c460bf0a43c6f82b07
dantalianSVP <spenceronepro88@gmail.com>
2019-08-21 17:27:21 +0300

    Сделано задание 6
    Сделана общая фикстура для все тестов
    Доработаны тесты
    создан новый тест для удаления контактов

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,16 +1,4 @@
-# -*- coding: utf-8 -*-
-from selenium import webdriver
-from fixture.application import Application
-import pytest
 from model.contact import Contact
-
-
-@pytest.fixture
-def app(request):
-    fixture = Application()
-    request.addfinalizer(fixture.destroy)
-    return fixture
-
 
 def test_add_contact(app):
     app.session.login(username="admin", password="secret")
--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,13 +1,4 @@
 from model.group import Group
-from fixture.application import Application
-import pytest
-
-
-@pytest.fixture
-def app(request):
-    fixture = Application()
-    request.addfinalizer(fixture.destroy)
-    return fixture
 
 
 def test_add_group(app):
@@ -15,8 +6,7 @@ def test_add_group(app):
     app.group.create(Group(name="One Group", header="MARK", footer="Farenheit"))
     app.session.logout()
 
-#def test_empty_group(app):
+# def test_empty_group(app):
 #    app.session.login(username="admin", password="secret")
 #    app.create_group( Group(name=" ", header=" ", footer=" "))
 #    app.session.logout()
-
